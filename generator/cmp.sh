bbc1aa3db0259f7b0fdc0d5487bcf202669b2220
tiger9tu <3098515559@qq.com>
2023-05-10 16:19:03 +0000

    ready to upload

--- generator/cmp.sh
+++
@@ -1,6 +0,0 @@
-
-echo "my output:"
-( export PATH=/workspace/sysu/bin:$PATH   CPATH=/workspace/sysu/include:$CPATH   LIBRARY_PATH=/workspace/sysu/lib:$LIBRARY_PATH   LD_LIBRARY_PATH=/workspace/sysu/lib:$LD_LIBRARY_PATH &&   clang -E /workspace/SYsU-lang/$1 |   clang -cc1 -ast-dump=json |   sysu-generator $2)
-printf "\n\n\n"
-echo "clang output:"
-( export PATH=/workspace/sysu/bin:$PATH   CPATH=/workspace/sysu/include:$CPATH   LIBRARY_PATH=/workspace/sysu/lib:$LIBRARY_PATH   LD_LIBRARY_PATH=/workspace/sysu/lib:$LD_LIBRARY_PATH && clang -E /workspace/SYsU-lang/$1 | clang -cc1 -O0 -S -emit-llvm | sed '1,5d' )
--- generator/dump.sh
+++
@@ -1,2 +0,0 @@
-( export PATH=/workspace/sysu/bin:$PATH   CPATH=/workspace/sysu/include:$CPATH   LIBRARY_PATH=/workspace/sysu/lib:$LIBRARY_PATH  
- LD_LIBRARY_PATH=/workspace/sysu/lib:$LD_LIBRARY_PATH &&   clang -E /workspace/SYsU-lang/$1 |   clang -cc1 -ast-dump$2 | sed '2,15d')
